1d84a657aa4c43a588b44f7cb7e8e86337749e73
RUPAM SADHUKHAN <115567972+GRUMPY-TUCKER@users.noreply.github.com>
2024-11-05 13:55:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import UseState from './useEffect';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <UseState />
+  </React.StrictMode>
+);
